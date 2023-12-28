849b7b5caf31cab9a24c86d34eda23b56bfa8f68
YonierGM <yoniermosquera55@gmail.com>
2023-12-28 18:01:20 -0500

    NAVBAR MOVIL

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  visible = false;
+  verLista = false;
+
+  menu(){
+    this.visible = !this.visible;
+  }
+
+  mostrarMenu(){
+    this.verLista = !this.verLista;
+  }
 
 }
